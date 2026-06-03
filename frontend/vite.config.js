e938b46b7b53b4d9457ca41748c0b9cd9c4c07e0
JidkiySolo <zhanchipov.rus@vsgaki.ru>
2026-06-03 13:44:39 +0800

    Add files via upload

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // ОБЯЗАТЕЛЬНО ДОЛЖНО БЫТЬ
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  },
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080', // Порт для локальной разработки
+        target: 'http://localhost:8080',
         changeOrigin: true,
       }
     }
